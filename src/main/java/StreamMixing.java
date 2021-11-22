c3c4915f12fcbc01d68c15c0d02bb6d1db762bad
NefedovAndry <nefedov_andry@bk.ru>
2021-11-22 23:04:50 +0200

    add new realisation of task#5

--- src/main/java/StreamMixing.java
+++ src/main/java/StreamMixing.java
@@ -5,11 +5,12 @@
 */
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class StreamMixing {
+/*public class StreamMixing {
 
     public static void main(String[] args) {
 
@@ -51,5 +52,20 @@ public class StreamMixing {
             result.add(secondToList.get(i));
         }
         return result.stream();
+    }
+}*/
+
+public class StreamMixing {
+    public static void main(String[] args) {
+        System.out.println((zip(Stream.of(1, 2, 3), Stream.of(4, 5, 6, 7, 8))).collect(Collectors.toList()));
+    }
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        Iterator<T> iteratorFirst = first.iterator();
+        Iterator<T> iteratorSecond = second.iterator();
+        Stream<T> result = Stream.empty();
+        while (iteratorFirst.hasNext() && iteratorSecond.hasNext()) {
+            result = Stream.concat(result, Stream.of(iteratorFirst.next(), iteratorSecond.next()));
+        }
+        return result;
     }
 }
